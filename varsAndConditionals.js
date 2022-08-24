106668dcf7f7ad83fc307f4a866f4d83e41b9e55
Sabrina Baccam <sabrinabaccam@gmail.com>
2022-08-23 21:25:54 -0500

    I'm really just testing this git stuff again

--- varsAndConditionals.js
+++ varsAndConditionals.js
@@ -1,5 +1,26 @@
 /*
     In this file, we'll be using variables, conditionals,
     and loops to play out a fight between Jon Snow and
-    Jamie Lannister.dslkfjdlsjdf
+    Jamie Lannister.
 */ 
+
+let jonSnowAttack = 25;
+let jamieLannisterAttack = 35;
+
+if (jonSnowAttack > jamieLannisterAttack) {
+    console.log("Jon Snow has better attack than Jamie Lannister");
+} else if (jamieLannisterAttack > jonSnowAttack){
+    console.log("Jamie Lannister has better attack than Jon Snow");
+} else {
+    console.log("Jon Snow and Jamie Lannister have the same attack");
+}
+
+let jonSnowHealth = 100;
+let jonSnowDefense = 0;
+
+if (jonSnowHealth <= jamieLannisterAttack) {
+    console.log("Jon Snow has been slain.");
+} else {
+    jonSnowHealth -= jamieLannisterAttack;
+    console.log(`Jon Snow's health is down to ${jonSnowHealth}`);
+}
